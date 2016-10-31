de289298666aec0ea913ba1ef5df828dd4336c24
Juan Carrera <mcabro@gmail.com>
2016-10-31 01:05:10 +0100

    Clean a comment, Check if dir exists, correct a typo and better final message.

--- php-jsonparser/parser.php
+++ php-jsonparser/parser.php
@@ -1,10 +1,11 @@
 <?php
 $jsonsDir="data/jsons";
 $jsonUrl='https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/';
-//$jsonOriginal = file_get_contents("data/eess_raw.json");
 
-if(!mkdir($jsonDir, 0755, true)) {
-    die('Fallo al crear las carpetas...');
+if (!is_dir($jsonsDir)) {
+    if(!mkdir($jsonsDir, 0755, true)) {
+        die('Fallo al crear las carpetas...');
+    }
 }
 $jsonOriginal = file_get_contents($jsonUrl);
 
@@ -13,5 +14,6 @@ foreach ( $jsonDecoded['ListaEESSPrecio']  as $eess ) {
     $filename='es::'. $eess['IDEESS'];
     file_put_contents( "$jsonsDir/$filename.json", json_encode($eess));
 }
-echo "Look for jsons in $resultsDir";
+echo "Look for jsons in $jsonsDir. Zip'em and use cbdocloader.";
+echo PHP_EOL;
 ?>

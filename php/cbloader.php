82330889f905e193eef1299bd961042e253ddf39
Juan Carrera <mcabro@gmail.com>
2016-11-11 00:21:16 +0100

    Primera versión funcional

--- php/cbloader.php
+++ php/cbloader.php
@@ -1,30 +1,59 @@
 <?php
 $shortoptions = 'b::';
-$shortoptions .= 'u:'; //URI: Either file:// or http(s)://
+$shortoptions .= 'u:';  //URI: Either filepath  or http(s)://
 $shortoptions .= 'h::'; //hostname
 $shortoptions .= 'p::'; //prepend
-
-//    -r  JSONroot              JSON root to parse
-//    -d  SearchKey            JSON Key ID
+$shortoptions .= 'r::'; //JSONroot
+$shortoptions .= 'k::';  //JSON key ID
+$shortoptions .= 'f::';  //format: arrayi|object
 
 $longoptions=array(
-    'help',	
+    'help',
 );
 
 $defaults = array(
     'b' => 'default',
     'h' => 'localhost',
     'p' => '',
+    'r' => null,
+    'k' => 'id',
+    'f' => 'array',
 );
 
 $options = getopt( $shortoptions, $longoptions ) + $defaults;
-$prepend = 'eess::';
 
-$jsonOriginal = file_get_contents($options['u']);
+$host = $options['h'];
+$dataOrigin = $options['u'];
+$bucketName = $options['b'];
+$prepend = $options['p'];
+$jsonRoot = $options['r'];
+$jsonKeyID = $options['k'];
+$format = $options['f'];
 
-$jsonDecoded = json_decode($jsonOriginal, true);
-foreach ( $jsonDecoded  as $item ) {
-    $key = $prepend . '';
-    
+$cluster = new CouchbaseCluster("couchbase://" . $host);
+$bucket = $cluster->openBucket($bucketName);
+
+
+$json = file_get_contents($dataOrigin);
+
+$data = json_decode($json);
+
+if ( !is_null($jsonRoot) && $format == 'object'  )
+  $data = $data[$jsonRoot];
+$counter = 0;
+$errors = 0;
+foreach ( $data  as $item ) {
+    $key = $prepend . '' . $item->$jsonRoot->$jsonKeyID;
+    $result = $bucket->upsert( $key, $item->$jsonRoot );
+    if ($result->error == 'NULL') {
+	echo 'Error: ' . $result->error . PHP_EOL;
+        $errors++;
+    } else {
+	echo 'CAS: ' . $result->cas . PHP_EOL;
+        $counter++;
+    }
 }
-?>
+
+echo PHP_EOL;
+echo 'Documents inserted: ' . $counter . ' - Errors: ' . $errors; 
+echo PHP_EOL;

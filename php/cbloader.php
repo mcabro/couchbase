fc42369024e3cc9c134cfeb95df6e8147bda00c4
Juan Carrera <mcabro@gmail.com>
2016-11-13 00:59:23 +0100

    Added dependency:GetOptionKit

--- php/cbloader.php
+++ php/cbloader.php
@@ -3,28 +3,52 @@ $shortoptions = 'b::';
 $shortoptions .= 'u:'; //URI: Either file:// or http(s)://
 $shortoptions .= 'h::'; //hostname
 $shortoptions .= 'p::'; //prepend
-
-//    -r  JSONroot              JSON root to parse
-//    -d  SearchKey            JSON Key ID
+$shortoptions .= 'r::'; //JSONroot
+$shortoptions .= 'k:'; //JSON key ID
 
 $longoptions=array(
-    'help',	
+    'help',
 );
 
 $defaults = array(
     'b' => 'default',
     'h' => 'localhost',
     'p' => '',
+    'r' => null,
 );
 
 $options = getopt( $shortoptions, $longoptions ) + $defaults;
-$prepend = 'eess::';
 
-$jsonOriginal = file_get_contents($options['u']);
+$host = $options['h'];
+$dataOrigin = $options['u'];
+$bucketName = $options['b'];
+$prepend = $options['p'];
+$jsonRoot = $options['r'];
+$jsonKeyID = $options['k'];
 
-$jsonDecoded = json_decode($jsonOriginal, true);
-foreach ( $jsonDecoded  as $item ) {
-    $key = $prepend . '';
-    
+
+$cluster = new CouchbaseCluster("couchbase://" . $host);
+$bucket = $cluster->openBucket(bucketName);
+
+$json = file_get_contents($dataOrigin);
+
+$data = json_decode($json);
+/*
+var_dump($options);
+echo PHP_EOL;
+var_dump($data);
+//*/
+if ( !is_null($jsonRoot) )
+  $data = $data[$jsonRoot];
+
+foreach ( $data  as $itemKey => $itemValue ) {
+    $key = $prepend . '' . $itemValue['$jsonKeyID'];
+    var_dump($key);
+    var_dump($itemValue);
+/*
+    $result = $bucket->upsert( $key, $value );
+    var_dump($result);
+//*/
 }
-?>
+//*/
+
